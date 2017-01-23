67a7fde4daf1e2ffb62194b0f8e10c17a2f8fbe8
arielRamati <ramatiari@gmail.com>
2017-01-23 21:53:31 +0200

    Add objects to the model & model params

--- src/model/Supplier.java
+++ src/model/Supplier.java
@@ -1,11 +1,51 @@
 package model;
 
+import model_params.PhoneNumber;
+
 public class Supplier {
 
 	private String firstName;
 	private String lastName;
-	
-	public Supplier() {
-		
+	private String address;
+	private PhoneNumber phoneNumber;
+
+	public Supplier(String firstName, String lastName, String adress, PhoneNumber phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = adress;
+		this.phoneNumber = phoneNumber;
 	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setPhoneNumber(PhoneNumber phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public PhoneNumber getPhoneNumber() {
+		return phoneNumber;
+	}
+
 }
--- src/model/WorkingHours.java
+++ src/model/WorkingHours.java
@@ -4,11 +4,66 @@ import java.sql.Date;
 
 public class WorkingHours {
 
-	private String employeeName;
+	private Employee employeeName;
 	private Date day;
 	private int enteringHour;
 	private int leavingHour;
-	
-	public WorkingHours() {
+	private String projectName;
+
+	public WorkingHours(Employee employee, Date date, int start, int end, String projectName){
+		this.employeeName = employee;
+		this.day = date;
+		this.enteringHour = start;
+		this.leavingHour = end;
+		this.projectName = projectName;
+	}
+
+	//deafult values for entering and leaving work
+	public WorkingHours(Employee employee, Date date, String projectName) {
+		this.employeeName = employee;
+		this.projectName = projectName;
+		this.day = date;
+		this.enteringHour = 7;
+		this.leavingHour = 4;
+	}
+
+	public Employee getEmployeeName() {
+		return employeeName;
+	}
+
+	public Date getDay() {
+		return day;
+	}
+
+	public int getEnteringHour() {
+		return enteringHour;
+	}
+
+	public int getLeavingHour() {
+		return leavingHour;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setEmployeeName(Employee employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public void setDay(Date day) {
+		this.day = day;
+	}
+
+	public void setEnteringHour(int enteringHour) {
+		this.enteringHour = enteringHour;
+	}
+
+	public void setLeavingHour(int leavingHour) {
+		this.leavingHour = leavingHour;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
 	}
 }
